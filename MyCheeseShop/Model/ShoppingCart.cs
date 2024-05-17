6cc7c65df0ef6c6e1aa817ba16d4722be7bd0f37
X <qbrext@gmail.com>
2024-05-17 10:34:50 +0100

    made the total price appear in on cartsummary

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -3,6 +3,7 @@ namespace MyCheeseShop.Model
 
     public class ShoppingCart
     {
+
         public event Action OnCartUpdated;
         private List<CartItem> _items; 
 
@@ -10,7 +11,29 @@ namespace MyCheeseShop.Model
         {
             _items = [];
         }
-  
+
+//CUSTOM
+        public double TotalCost()
+        {
+            double price = 0;
+            double total_cost = 0;
+            var amount_weight = 0;
+
+            var items = _items;
+            items.ToList();
+            foreach (var item in items)
+            {
+                amount_weight += item.Quantity;
+                price = (amount_weight / 2) * item.Cheese.Price;
+                total_cost += price;
+            }
+
+            return price;
+
+        }
+
+//CUSTOM
+
         public void AddItem(Cheese cheese, int quantity)
         {
             var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
